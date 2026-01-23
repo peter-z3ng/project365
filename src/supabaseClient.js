4950e3b40edba38f5161ee3c96452ea2298e7b83
Paing Thit Xan <paingthitxan184@gmail.com>
2026-01-23 15:12:57 +0800

    Supabase Project ID and Publishable Key added

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
+  "https://duftzvdkarwuoqwvymgr.supabase.co",
+  "sb_publishable_qCfSoLChWWfcG6vdvTvYUg_b092HEDc"
 );
